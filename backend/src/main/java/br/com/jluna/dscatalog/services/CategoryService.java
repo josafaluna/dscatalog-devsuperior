0f87627b82b907d6a16c7901fc187d8e30c1f349
Josafá Luna <josafaluna@gmail.com>
2021-11-07 11:38:49 -0300

    Insert Category

--- backend/src/main/java/br/com/jluna/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/jluna/dscatalog/services/CategoryService.java
@@ -35,4 +35,15 @@ public class CategoryService {
 		return new CategoryDTO(category);
 	}
 
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+
+		Category entity = new Category();
+		entity.setName(dto.getName());
+
+		entity = repository.save(entity);
+
+		return new CategoryDTO(entity);
+	}
+
 }
